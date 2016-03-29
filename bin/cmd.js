f30b4170d77176dbc8fefd6e833a3712700cbd72
azu <azuciao@gmail.com>
2016-03-29 10:05:44 +0900

    fix(cli): remove fs

--- bin/cmd.js
+++ bin/cmd.js
@@ -2,14 +2,17 @@
 var concat = require('concat-stream');
 var slug = require("slug");
 var string = process.argv[2];
-var input = string && string !== '-'
-        ? fs.createReadStream(process.argv[2])
-        : process.stdin
-    ;
-input.pipe(concat(function (buf) {
-    var targetString = buf.toString('utf8');
-    if (targetString == null || targetString.length === 0) {
-        throw new Error("Usage: slug-command 'string for slug.'");
-    }
-    console.log(slug(targetString, {lower: true}));
-}));
+function slugC(string) {
+    console.log(slug(string, {lower: true}))
+}
+if (process.argv[2]) {
+    slugC(process.argv[2]);
+} else {
+    process.stdin.pipe(concat(function (buf) {
+        var targetString = buf.toString('utf8');
+        if (targetString == null || targetString.length === 0) {
+            throw new Error("Usage: slug-command 'string for slug.'");
+        }
+        slugC(targetString);
+    }));
+}
